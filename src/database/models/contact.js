3adb85512859f215be24390b2ef042988f404b81
phunmbi <phunmbi@gmail.com>
2019-07-26 18:42:32 +0100

    chore(setup): setup the database
    - configure a postgres db and interface it with sequelize ORM
    - setup all necessary models with required associations between models
    
    [Finishes #167433131]

---
+++ src/database/models/contact.js
@@ -0,0 +1,34 @@
+export default (sequelize, DataTypes) => {
+  const Contact = sequelize.define(
+    'Contact',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { paranoid: true }
+  );
+  Contact.associate = (models) => {
+    Contact.hasMany(models.Message, {
+      foreignKey: 'senderId',
+      as: 'messagesSent'
+    });
+
+    Contact.hasMany(models.Message, {
+      foreignKey: 'receiverId',
+      as: 'messagesReceived'
+    });
+
+  };
+  return Contact;
+};
